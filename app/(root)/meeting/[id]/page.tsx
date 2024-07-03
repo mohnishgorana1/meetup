d06a5b8e0b5bc85a417b9c2c46a3c21fcb178155
mohnishgorana1 <mohnishgorana1@gmail.com>
2024-07-03 20:39:15 +0530

    File Folder structure completed now lets move to develop ui component

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+function Meeting({ params }: { params: { id: string } }) {
+  return (
+    <div>Meeting #{params.id}</div>
+  )
+}
+
+export default Meeting
