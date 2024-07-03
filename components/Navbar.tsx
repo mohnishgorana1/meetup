533f92b63f5b507c8cee90e74885e802ef70e9f6
mohnishgorana1 <mohnishgorana1@gmail.com>
2024-07-03 21:54:54 +0530

    navigation and side done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='flex items-center justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href={"/"} className='flex items-center gap-2'>
+        <Image src={'/icons/logo.svg'} width={32} height={32} alt='logo' className='max-sm:size-10' />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden '>Meetup</p>
+      </Link>
+      <div className="flex items-center justify-between gap-5">
+        {/* Clerk User Management */}
+
+        <MobileNav />
+      </div>
+
+    </nav>
   )
 }
 
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,35 @@
+'use client'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 function Sidebar() {
+  const pathName = usePathname()
   return (
-    <div>Sidebar</div>
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+      <div className="flex flex-1 flex-col gap-6">
+        {
+          sidebarLinks.map((link, idx) => {
+            // const isActive = pathName === link.route || pathName.startsWith(link.route)
+            const isActive = pathName === link.route 
+            return (
+              <Link
+                key={idx} href={link.route}
+                className={`${isActive && "bg-blue-1"} flex gap-4 items-center p-4 rounded-lg justify-start`}
+              >
+                <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+                <p className='text-lg font-semibold max-lg:hidden'>
+                  {link.label}
+                </p>
+              </Link>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
